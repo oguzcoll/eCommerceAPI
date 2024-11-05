6141233f00afaa757dc55d629df2be145cda934d
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2024-11-05 18:07:55 +0300

    basic setup

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const connectDB = require('./db/connect');
+const morgan = require('morgan');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const app = express();
+const port = process.env.PORT || 6000;
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
