4b6195ffcb6e7b9aa6e45431e36772d4bd61b9fc
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2024-11-06 14:21:59 +0300

    register controller

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ const authRouter = require('./routes/authRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const app = express();
-const port = process.env.PORT || 6000;
-app.use(morgan('tiny'));
+const port = process.env.PORT || 4000;
 app.use(express.json());
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
 app.use('/api/v1/auth', authRouter);
+app.use(morgan('tiny'));
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
