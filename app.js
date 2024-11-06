8c84d72e4ad22cc01b39b24d2ba94f62653207f8
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2024-11-06 12:57:41 +0300

    auth routes structure implemented

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 const express = require('express');
 const connectDB = require('./db/connect');
 const morgan = require('morgan');
+const authRouter = require('./routes/authRoutes');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const app = express();
@@ -14,7 +15,7 @@ app.use(errorHandlerMiddleware);
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
+app.use('/api/v1/auth', authRouter);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
