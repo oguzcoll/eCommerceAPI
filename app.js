2f1ed071146c189442f024a8adb2c2b4ccef89a6
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2024-11-09 20:28:06 +0300

    User Routes Structure added

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express');
 const connectDB = require('./db/connect');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+
 const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.get('/', (req, res) => {
@@ -20,9 +25,11 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 app.use(morgan('tiny'));
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
